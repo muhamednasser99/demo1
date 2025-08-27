86c526dcb83f946f4e54827140c3110589ad5d5e
muhamednasser99 <mn265944@gmail.com>
2025-08-27 18:45:32 +0300

    adding new person details for testing webhook feature

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -39,6 +39,13 @@ public class PersonService {
 		p.setFirstName("AbdulHalim");
 		p.setLastName("Mahmoud");
 		persons.add(p);
+
+		p = new Person();
+		p.setId("5");
+		p.setAge(27);
+		p.setFirstName("Webhook");
+		p.setLastName("Testing");
+		persons.add(p);
 		
 	}
 	public Person getPerson(String id) {
